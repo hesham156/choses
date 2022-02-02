dfd42060dd18f81856c6803b09e2fb432341d485
hesham156 <h456ad@gmail.com>
2022-02-01 22:05:33 -0800

    clic

--- js/index.js
+++ js/index.js
@@ -13,3 +13,25 @@ const cl = ()=>{
 
     }
 }
+const encrm = ()=>{
+    localStorage.setItem('card', JSON.parse(localStorage.getItem('card'))+1)
+    JSON.parse(localStorage.getItem('card'))
+   
+
+    
+    console.log(JSON.parse(localStorage.getItem('card')))
+}
+// let d =document.createElement('div');
+// let ca = document.getElementById('ca');
+let nm =document.getElementById('num')
+nm.innerHTML=JSON.parse(localStorage.getItem('card'));
+window.addEventListener('click',()=>{
+    // d.appendChild(document.createTextNode(JSON.parse(localStorage.getItem('card'))));
+    // bar.appendChild(d);
+    nm.innerHTML=JSON.parse(localStorage.getItem('card'));
+
+})
+// ca.addEventListener('click',()=>{
+//     // window.location.reload();
+// })
+
