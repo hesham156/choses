135883ec773be69786200df1518225abfb668d0f
hesham156 <69813818+hesham156@users.noreply.github.com>
2023-08-23 10:17:38 -0700

    Update ssss.js

--- js/ssss.js
+++ js/ssss.js
@@ -1,2 +1,6 @@
 
-console.log('hello');
+console.log('helloagain');
+const meta = document.createElement('meta');
+meta.name='google-site-verification'
+meta.content="xsrkdjtyj2GBBe1XFOPKFTgI0d4sZv0lXK5vozmOiGM"
+document.head.appendChild(meta)
